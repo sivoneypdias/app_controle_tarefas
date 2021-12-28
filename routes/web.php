a8d0acc464584dec57dc75fc37ea05dcb9399d1b
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-28 16:59:53 -0300

    Verificação de e-mail (MyVerifyEmail)

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// https://laravel.com/docs/8.x/verification#protecting-routes
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+->name('home')
+->middleware('verified');
 
-Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController')->middleware('verified');
